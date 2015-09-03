a92bccd24aa1bf47d37b4bdb1edf9606a6b24ecd
aminami1127 <aminami1127@users.noreply.github.com>
2015-09-03 14:46:04 +0900

    mod

--- generator/ex2.py
+++ generator/ex2.py
@@ -5,5 +5,5 @@ def sample_generator(start, end):
     for i in range(start, end + 1):
         yield i ** 2
 
-sample = sample_generator(1, 10)
+sample = sample_generator(1, 10)  # generatorを呼び出すとiteratorオブジェクトが返される
 print([x for x in sample])
